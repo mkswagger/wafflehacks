02533ff4b3fa345afef87a4fffea8c7d3326832e
aviiciii <laavesh1@gmail.com>
2023-06-25 12:49:14 +0530

    ssl ceritificate

--- Model/text_classifier.py
+++ Model/text_classifier.py
@@ -1,4 +1,14 @@
 import nltk
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 
