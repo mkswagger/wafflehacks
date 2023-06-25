68d62e60576f1fbecf559cfe9dff74a600c5d713
aviiciii <laavesh1@gmail.com>
2023-06-25 14:23:02 +0530

    refactor

--- server/app.py
+++ server/app.py
@@ -20,33 +20,37 @@ def hello_world():
 def check_text():
     data = request.get_json()
     text = data['text']
-
+    
     if text is None:
         return jsonify({'error': 'No text provided'})
 
-    # split text into sentences by punctuation . ! and ?
-
+    # split text into sentences
     sentences = split_sentences(text)
     
     print(sentences)
 
-    # check each sentence
     result = []
+
+    # check each sentence and append to result
     for sentence in sentences:
         sentence = sentence.strip()
-        # if sentence is too long, split it into smaller sentences
+
+        # if sentence is too long, split it into smaller sentences and check each one and append to result as single sentence
         if len(sentence) > 15:
             bad = False
+
             while len(sentence) > 15:
                 if perform_sentiment_analysis(sentence[:15]) == "offensive":
                     bad = True
                     break
-
                 sentence = sentence[15:]
+
             if perform_sentiment_analysis(sentence) == "offensive":
                 bad = True
+
             result.append("offensive" if bad else "not offensive")
 
+        # if sentence is short enough, check it and append to result
         else:
             result.append(perform_sentiment_analysis(sentence))
 
